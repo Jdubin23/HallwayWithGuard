72c13eb834f88b17948af6b0b45f1b6e81441b43
Jman87 <juanjomartinezv2707@gmail.com>
2026-03-02 01:03:08 -0500

    still work in progress

--- HallwayGuard/Assets/Scripts/Enemies/Observer.cs
+++ HallwayGuard/Assets/Scripts/Enemies/Observer.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
-using System.Collections;
-
 
 public class Observer : MonoBehaviour
 {
     public Transform Player; // Reference to the player's transform
-    public SmallGuard Guard; // Reference to the SmallGuard script to communicate player detection status
+    public SmallGuard Guard; // Reference to the big guard script to communicate player detection status
     public bool PlayerInRange;
 
     void OnTriggerEnter(Collider other)
@@ -13,7 +11,6 @@ public class Observer : MonoBehaviour
         if (other.transform == Player)
         {
             PlayerInRange = true; // Player has entered the observer's range
-            StartCoroutine(chaseTiming());
         }
     }
 
@@ -22,35 +19,24 @@ public class Observer : MonoBehaviour
         if (other.transform == Player)
         {
             PlayerInRange = false;
-            // Player has left the observer's range
-            Debug.Log("Player Lost!");
+            Guard.StartChaseTimer(); // Player has left the observer's range
         }
     }
-
-    IEnumerator chaseTiming ()
-    {
-        yield return new WaitForSeconds(2f);
-
-        if (PlayerInRange)
-        {
-            Guard.Chase();
-        }
-    }
-
-
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!PlayerInRange) return;
-
-
-        if (PlayerInRange)
+        
+        
+        if(PlayerInRange)
         {
             Vector3 Origin = transform.position; // Ray starts from the guards position
             Vector3 direction = Player.position - Origin;
@@ -64,8 +50,8 @@ public class Observer : MonoBehaviour
                 // If it hits the Guard (the parent), it will fail this 'if'
                 if (hit.collider.transform == Player)
                 {
-                    Debug.Log("Prepare for dash!");
-                    chaseTiming();
+                    Debug.Log("Player Detected by Observer");
+                    Guard.Chase();
                 }
                 else
                 {
--- HallwayGuard/Assets/Scripts/Enemies/SmallGuard.cs
+++ HallwayGuard/Assets/Scripts/Enemies/SmallGuard.cs
@@ -10,101 +10,100 @@ public class SmallGuard : MonoBehaviour
     
     public Transform Player; // Reference to the player's transform
     public Observer Observer; // Reference to the Observer script to communicate player detection status
-    
+
     [Header("Enemy Settings")]
     public float Speed = 3f;
     public NavMeshAgent Agent; // Reference to the NavMeshAgent component
-    public Transform[] Waypoints; // Array of patrol points for the guard to move between
-    int m_CurrentWaypointIndex; 
+    public Transform[] Waypoints2; // Array of patrol points for the guard to move between
+    int m_CurrentWaypointIndex;
     public Collider VisionCollider; // Collider used to detect the player within the guard's vision range
     public bool PlayerDetected = false;
-    public float ChaseDuration;
-    public float CautionDuration;
 
-    [Header("Dash Settings")]
-    // public float dashSpeed;
-    public float dashDuration;
-    private Vector3 dashDirection;
-    public float stunDuration;
-    private float dashTimer;
-    private Vector3 savedPosition; //last saved player position
-    public bool isDashing;
-    
+    [Header("Chase Settings")]
+    private float ChaseDuration = 2;
+    private float CautionDuration;
+    public float chaseDelay;
+    private Vector3 lastLocation; //last location player was seen in
+    private Vector3 chaseDirection;
+    public float chaseSpeed;
 
-    public LayerMask Wall;
-    
-    
 
     [Header("State Settings")] // Bools for what state the guard is currently in. Might make these separate classes inheriting the behavior depending on the how the script develops
     public bool IsChasing = false; // Only enables once the player is detected, disables patrol.
     public bool IsPatrolling = true; // Default State, only deactivates once chasing is enabled.
     public bool LostPlayer = false; // Activates once the player is lost, disables chase.
     public bool IsCautious = false; // Activates once the player is lost, disables chase, activates caution.
+    
 
-
-
+    #region Awake, Start, Update
     private void Awake()
     {
         Player = GameObject.Find("Player").transform; // Finds the player in the scene and assigns it to the Player variable
         // will be useful for chasing the player
     }
-    
-    
+
+    void Start()
+    {
+        Agent.SetDestination(Waypoints2[0].position); // Start patrolling towards the first waypoint
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPatrolling)
+            {
+                Patrol();
+            }
+        if (IsChasing)
+            {
+                transform.position += chaseDirection * chaseSpeed * Time.deltaTime;
+                
+                //Invoke(nameof(Chase), chaseDelay); // Chase the player
+            }
+    }
+    #endregion
+
     #region State Behaviors
     private void Patrol()
     {
+        if (!Agent.enabled) return;
+        
         if (IsPatrolling)
         {
-            if(Agent.remainingDistance < Agent.stoppingDistance)
+            if (Agent.remainingDistance < Agent.stoppingDistance)
             {
-                m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % Waypoints.Length; // Moves guard to the following waypoints
-                Agent.SetDestination(Waypoints[m_CurrentWaypointIndex].position);
-            } 
+                m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % Waypoints2.Length; // Moves guard to the following waypoints
+                Agent.SetDestination(Waypoints2[m_CurrentWaypointIndex].position);
+            }
         }
-        
+
     }
     public void Chase()
     {
         CancelInvoke(nameof(Caution));
+        Agent.speed = 6f; // Use Agent.speed to actually change NavMesh movement
         IsPatrolling = false;
-        IsChasing = true;
-        Agent.speed = 10f; // Use Agent.speed to actually change NavMesh movement
+        
+        IsChasing = true; // You need to set this to true so Update() knows to follow the player
 
+        lastLocation = Player.position;
 
-        // Chase behavior is a single, fast dash to the player.
-        Agent.enabled = false; // disable navmesh.
+        chaseDirection = (lastLocation - transform.position).normalized;
 
+        transform.forward = chaseDirection;
         
+        Agent.enabled = false;
         
-        dashTimer = dashDuration;
-        savedPosition = Player.position;
-        
-        dashDirection = (savedPosition - transform.position).normalized;
-        transform.forward = dashDirection;
-        
-        
-        //transform.position += dashDirection * Agent.speed * Time.deltaTime;
-        //dashTimer -= Time.deltaTime;
-        
-        
-        /*
-        if (dashTimer <= 0f)
-        {
-            Stun();
-        }
-        */
-
-        // Remove the 'if (Observer.PlayerInRange)' line
-        isDashing = true; // You need to set this to true so Update() knows to follow the player
-        
+        StartCoroutine(ChaseDelay());
         
     }
-    void Dash()
+
+    IEnumerator ChaseDelay()
     {
-        transform.position += dashDirection * 10f * Time.deltaTime;
-    }
+        yield return new WaitForSeconds(chaseDelay);
 
-    
+        IsChasing = true;
+    }
     public void StartChaseTimer()
     {
         // Only start the countdown if we are currently chasing
@@ -118,66 +117,40 @@ public class SmallGuard : MonoBehaviour
 
         IsChasing = false; // cancels chase
         IsPatrolling = true; // makes patrolling true
+        Agent.enabled = true;
         Agent.speed = Speed; // Reset speed to default
     }
 
     public void Stun()
     {
-        Debug.Log("Brother is Stunned! Get out of there!");
         IsChasing = false;
-        IsPatrolling = false; // both states are false for a bit.
-        
-        Invoke(nameof(Patrol), stunDuration);
-        // being stunned means 
+
+        Agent.enabled = true;
+        IsPatrolling = true;
+
+        Agent.SetDestination(Waypoints2[m_CurrentWaypointIndex].position);
     }
     #endregion
-    
 
-    void Start()
-    {
-        Agent.SetDestination(Waypoints[0].position); // Start patrolling towards the first waypoint
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (IsPatrolling)
-        {
-            Patrol();
-        }
-        if (IsChasing)
-        {
-            Chase();
-        }
-        if (isDashing)
-        {
-            Dash();
-        }
-        
-
-    }
     //when colliding with player, End game or trigger capture state
     void OnCollisionEnter(Collision collision)
     {
-        if (IsChasing)
+        // Check if the object we bumped into is the player
+        if (!IsChasing)
         {
-            /*
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
-            {
-                Stun();
-            }
-            */
-
-
-            if (collision.transform == Player)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-            } 
+            return;
         }
         
-        // Check if the object we bumped into is the player
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Stun();
+        }
         
+        if (collision.transform == Player)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+        }
     }
 
     
